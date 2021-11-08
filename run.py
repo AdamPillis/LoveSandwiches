aea143559280353ae724d1da79b2b985b03868c8
AdamPillis <adam.pillis@hotmail.com>
2021-11-08 10:49:13 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,7 +25,7 @@ def get_sales_data():
         print('Data should be six numbers, separated by commas.')
         print('Example: 10,20,30,40,50,60\n')
 
-        data_str = input('Enter you data here: ')
+        data_str = input('Enter you data here:\n')
 
         sales_data = data_str.split(',')
         validate_data(sales_data)
@@ -34,7 +33,7 @@ def get_sales_data():
         if validate_data(sales_data):
             print('Data is valid')
             break
-    
+
     return sales_data
 
 
@@ -53,7 +52,7 @@ def validate_data(values):
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
         return False
-    
+
     return True
 
 
@@ -79,12 +78,12 @@ def calculate_surplus_data(sales_row):
     print('Calculating surplus data...\n')
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
-    
+
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
 
 
@@ -102,7 +101,7 @@ def get_last_5_entries_sales():
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    
+
     return columns
 
 
@@ -118,7 +117,7 @@ def calculate_stock_data(data):
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
-    
+
     return new_stock_data
 
 
@@ -138,4 +137,3 @@ def main():
 
 print('Welcome to Love Sandwiches Data Automation')
 main()
-
